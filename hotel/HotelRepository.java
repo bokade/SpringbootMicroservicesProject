350ec0c130f4b8b1ae4ab3df1cc2138d3ee68fda
Swapnil Bokade <121025630+bokade@users.noreply.github.com>
2023-11-19 13:55:10 +0530

    Create HotelRepository.java

---
+++ hotel/HotelRepository.java
@@ -0,0 +1,11 @@
+package com.example.repository;
+
+import com.example.model.Hotel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HotelRepository extends MongoRepository<Hotel,String>
+{
+
+}
