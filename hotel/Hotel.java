49a5b02edb3bbe769602780c1c5549bf7cce97df
Swapnil Bokade <121025630+bokade@users.noreply.github.com>
2023-11-19 13:54:46 +0530

    Create Hotel.java

---
+++ hotel/Hotel.java
@@ -0,0 +1,25 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "hotels")
+public class Hotel
+{
+    @Id
+    private String id;
+    private String name;
+    private String location;
+    private String about;
+
+
+
+}
