74bd5417415dee0842a3dd9fe27b2b259dc3668e
Swapnil Bokade <121025630+bokade@users.noreply.github.com>
2023-11-19 14:04:03 +0530

    Create RatingService.java

---
+++ user/RatingService.java
@@ -0,0 +1,27 @@
+package com.example.UserService.external.service;
+
+import com.example.UserService.model.Rating;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import java.util.Map;
+@Service
+@FeignClient(name="RATING-SERVICE")
+public interface RatingService
+{
+      @PostMapping("/ratings")
+      public ResponseEntity<Rating> createRating(Rating values);
+
+      @PutMapping("ratings/{ratingId}")
+      public ResponseEntity<Rating> updatRating(@PathVariable("ratingId") String ratingId, Rating rating);
+
+      @DeleteMapping("/ratings/{ratingId}")
+      public void deleteRating(@PathVariable String ratingId);
+
+
+}
